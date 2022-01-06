398d84ee64c3d713977bfa3602b9e9c315659c05
Guilheeeerme <guilherme_moura.10222@outlook.com>
2022-01-05 22:16:31 -0300

    feat: add AuthenticateDeliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,21 @@ import { CreateClientController } from "./modules/clients/useCases/createClient/
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
+
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 
-routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post(
+  "/deliveryman/authenticate",
+  authenticateDeliverymanController.handle
+);
 
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryman/", createDeliverymanController.handle);
